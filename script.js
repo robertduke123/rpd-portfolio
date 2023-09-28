e893be191cd4f4ecef730de39113a10bc7595828
Robert Duke <robduke123@gmail.com>
2023-09-28 16:05:27 +1300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+
+// });
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
