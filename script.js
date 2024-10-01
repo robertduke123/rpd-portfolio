809c6acb970374b4869a153f3b72e12644930f78
Robert Duke <robduke123@gmail.com>
2024-10-01 17:21:14 +1300

    updating description

--- script.js
+++ script.js
@@ -10,37 +10,55 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-let counter = 0
-let sum = 0
+const arr = [5,6,1,4,3,9,2,5,6,9,8,3,7,1,207,99,652,45,31,256,789,100]
 
-function fibanatchiRecurcion(num) {  
-  if(num < 2){
-    return num
+function sorting(arr){
+  let tempI = 0
+  let tempJ = 0 
+  for(let i = 0 ; i < arr.length; i++){
+    for(let j = 0; j < arr.length; j++){
+      if(arr[i] < arr[j]){
+        tempI = arr[i]
+        tempJ = arr[j]
+        arr[j] = tempI
+        arr[i] = tempJ 
+      }
+    }
   }
-
-  return fibanatchiRecurcion(num-1) + fibanatchiRecurcion(num-2)  
+  return arr
 }
 
-console.log(fibanatchiRecurcion(6));
-
-
-function fibanatchiIterative(num) {
-  let first = 0
-  let second = 1
-  if(num === 0){
-    return first
-  } else if(num <= 2){
-    return second
+function selectionSort(arr) {
+  for(let i = 0; i < arr.length; i++){    
+  let temp = arr[i]
+	let smallest = i;
+    for (let j = i+1; j < arr.length; j++){
+      if(arr[j] < arr[smallest]){
+        smallest = j
+      }     
+    }
+    arr[i] = arr[smallest]
+    arr[smallest] = temp   
   }
-  let sum
-	for(let i = 2; i <= num; i++){
-    sum = first + second
-    first = second
-    second = sum
-  }
-  return sum
+  return arr
 }
 
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-ggg
-// console.log(fibanatchiIterative(7))
+function mergeSort(array) {
+	if (array.length === 1) {
+		return array;
+	}
+	// Split Array in into right and left
+
+	return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {}
+
+const answer = mergeSort(numbers);
+console.log(answer);
+
+console.log(sorting(arr));
+
+console.log(selectionSort(arr));
